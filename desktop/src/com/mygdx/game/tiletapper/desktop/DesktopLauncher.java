a4e515c6aa258e9f3d2f39a3810993085e284d1c
Pegasuraptor <jake.rowe@hotmail.co.uk>
2015-01-18 00:15:16 +0000

    Initial Commit

---
+++ desktop/src/com/mygdx/game/tiletapper/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.game.tiletapper.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.tiletapper.TileTapper;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TileTapper(), config);
+	}
+}
